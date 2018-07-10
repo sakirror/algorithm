2daeb9d8c7a3190bda89600580e190c406bfd211
Akira Sato <satoach.net@example.com>
2018-07-10 15:08:24 +0900

    add printOperation

--- levenshtein_distance/main.go
+++ levenshtein_distance/main.go
@@ -29,13 +29,16 @@ func prepareMatrix(rowstr string, columnstr string) [][]Matrix {
 	for i := 0; i < row; i++ {
 		matrix[i] = make([]Matrix, column)
 		matrix[i][0].distance = i
+		matrix[i][0].op = DEL
 	}
 	for i := 0; i < column; i++ {
 		matrix[0][i].distance = i
+		matrix[0][i].op = INS
 	}
 
-	for i := 0; i < row; i++ {
-		for j := 0; j < column; j++ {
+	matrix[0][0].op = NOSET
+	for i := 1; i < row; i++ {
+		for j := 1; j < column; j++ {
 			matrix[i][j].op = NOSET
 		}
 	}
@@ -63,6 +66,34 @@ func printMatrix(rowstr string, columnstr string, matrix [][]Matrix) {
 			fmt.Printf("%02d%c ", m.distance, m.op)
 		}
 		fmt.Println()
+	}
+}
+
+func printOperation(rowstr string, columnstr string, matrix [][]Matrix) {
+	row := len(matrix) - 1
+	column := len(matrix[0]) - 1
+	target := func() string {
+		return fmt.Sprintf("%s[%d]", rowstr, row-1)
+	}
+
+	for row > 0 || column > 0 {
+		switch matrix[row][column].op {
+		case INS:
+			fmt.Printf("%s insert '%c'\n", target(), columnstr[column-1])
+			column--
+		case DEL:
+			fmt.Printf("%s delete '%c'\n", target(), rowstr[row-1])
+			row--
+		case REP:
+			if matrix[row][column].distance != matrix[row-1][column-1].distance {
+				fmt.Printf("%s replace '%c' to '%c'\n", target(), rowstr[row-1], columnstr[column-1])
+			}
+			column--
+			row--
+		default:
+			fmt.Printf("[%d][%d] is invalid operation %c.\n", row, column, matrix[row][column].op)
+			break
+		}
 	}
 }
 
@@ -106,19 +137,20 @@ func LevenshteinDistance(rowstr string, columnstr string, matrix [][]Matrix) (in
 }
 
 func main() {
-	row := "kitten"
-	column := "sitting"
+	rowstr := "kitten"
+	columnstr := "sitting"
 
 	if len(os.Args) > 2 {
-		row = os.Args[1]
-		column = os.Args[2]
+		rowstr = os.Args[1]
+		columnstr = os.Args[2]
 	}
 
-	matrix := prepareMatrix(row, column)
+	matrix := prepareMatrix(rowstr, columnstr)
 
-	if distance, err := LevenshteinDistance(row, column, matrix); err == nil {
-		fmt.Printf("'%s' - '%s' distance is %d\n", row, column, distance)
-		printMatrix(row, column, matrix)
+	if distance, err := LevenshteinDistance(rowstr, columnstr, matrix); err == nil {
+		fmt.Printf("'%s' -> '%s' distance is %d\n", rowstr, columnstr, distance)
+		printMatrix(rowstr, columnstr, matrix)
+		printOperation(rowstr, columnstr, matrix)
 	} else {
 		fmt.Printf("err: %s", err)
 	}
